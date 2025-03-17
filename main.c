846ddab54360d376fcf621c0dc26ae1b4bfcf088
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-03-17 13:14:36 +0100

    main added

---
+++ main.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmeubrin <lmeubrin@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/17 13:14:02 by lmeubrin          #+#    #+#             */
+/*   Updated: 2025/03/17 13:14:04 by lmeubrin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "include/miniRT.h"
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	//what it should do:
+	// t_scene scene;
+	//
+	// if (argc != 2)
+	// 	return (0);
+	// scene = parse_scene(argv[1]);
+	// render_scene(scene);
+	return (0);
+}
