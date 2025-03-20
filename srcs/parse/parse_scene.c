3c70993b44bcd5e904f81983dbfe40e65fb9b9d3
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-03-20 14:57:54 +0100

    removed uneccessary functions for norm

--- srcs/parse/parse_scene.c
+++ srcs/parse/parse_scene.c
@@ -26,32 +26,6 @@ int	parse_scene(char *filename, t_scene *scene)
 	return (0);
 }
 
-// return 0 on error, -1 if no camera, 1 if camera
-int	get_camera(char *line, t_camera *camera, int *got_camera)
-{
-	if (line[0] == 'C' && line[1] == ' ')
-	{
-		if (!set_camera(line, camera))
-			return (0);
-		++(*got_camera);
-		return (1);
-	}
-	return (-1);
-}
-
-// return 0 on error, -1 if no ambient, 1 if ambient
-int	get_ambient(char *line, t_ambient *ambient, int *got_ambient)
-{
-	if (line[0] == 'C' && line[1] == ' ')
-	{
-		if (!set_ambient(line, ambient))
-			return (0);
-		++(*got_ambient);
-		return (1);
-	}
-	return (-1);
-}
-
 int	parse_file(int fd, t_scene *scene)
 {
 	char	*line;
@@ -67,10 +41,10 @@ int	parse_file(int fd, t_scene *scene)
 			break ;
 		if (line[0] == '\n')
 			continue ;
-		if (!get_camera(line, &(scene->camera), &got_camera))
-			return (0);
-		else if (!get_ambient(line, &(scene->ambient), &got_ambient))
-			return (0);
+		if (!ft_strncmp(line, "A ", 2))
+			++got_ambient;
+		else if (!ft_strncmp(line, "C ", 2))
+			++got_camera;
 		else if (!ft_strncmp(line, "L ", 2))
 			scene->light_count++;
 		else if (!ft_strncmp(line, "sp ", 3))
@@ -81,8 +55,6 @@ int	parse_file(int fd, t_scene *scene)
 			scene->cylinder_count++;
 		free(line);
 	}
-	printf("got_camera: %d\n", got_camera);
-	printf("got_ambient: %d\n", got_ambient);
 	if (got_camera != 1 || got_ambient != 1)
 		return (ft_parseerror("need 1 camera (C) & 1 ambient light (A)", NULL));
 	get_next_line(-1);
@@ -100,8 +72,10 @@ int	set_obj(char *line, t_scene *scene, int *indexi)
 		return (set_plane(line, &(scene->planes[indexi[PLANE]++])));
 	else if (!ft_strncmp(line, "cy ", 3))
 		return (set_cylinder(line, &(scene->cylinders[indexi[CYLINDER]++])));
-	else if (!ft_strncmp(line, "A ", 2) || !ft_strncmp(line, "C ", 2))
-		return (1);
+	else if (!ft_strncmp(line, "A ", 2))
+		return (set_ambient(line, &(scene->ambient)));
+	else if (!ft_strncmp(line, "C ", 2))
+		return (set_camera(line, &(scene->camera)));
 	return (ft_parseerror("invalid object", line));
 }
 
