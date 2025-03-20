ac050ee6a584be8f5896eece96befa96292e8e79
kjzl <kjzl@posteo.de>
2025-03-20 16:01:44 +0100

    change intersect for sphere

--- srcs/sphere.c
+++ srcs/sphere.c
@@ -6,7 +6,7 @@
 /*   By: kwurster <kwurster@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 11:01:02 by kwurster          #+#    #+#             */
-/*   Updated: 2025/03/19 14:45:13 by kwurster         ###   ########.fr       */
+/*   Updated: 2025/03/20 15:54:40 by kwurster         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,26 +20,47 @@ t_vec3	sphere_normal(t_sphere sphere, t_vec3 point)
 bool	sphere_intersect(t_sphere *sphere, t_ray ray, t_intersection *out)
 {
 	t_vec3			oc;
-	float			ahc[3];
+	float			abc[3];
 	float			discriminant;
 	float			sqrt_d;
 	float			t;
 
-	oc = vec3_subtract(ray.origin, sphere->pos);
-	ahc[0] = vec3_squared_length(ray.direction);
-	ahc[1] = vec3_dot(oc, ray.direction);
-	ahc[2] = vec3_squared_length(oc) - sphere->radius * sphere->radius;
-	discriminant = ahc[1] * ahc[1] - ahc[0] * ahc[2];
+	(void)t;
+	// L
+	oc = vec3_subtract(sphere->pos, ray.origin);
+	// a
+	abc[0] = vec3_squared_length(ray.direction);
+	// b
+	abc[1] = 2 * vec3_dot(oc, ray.direction);
+	// c
+	abc[2] = vec3_squared_length(oc) - sphere->radius * sphere->radius;
+	discriminant = abc[1] * abc[1] - 4 * abc[0] * abc[2]; // b*b - 4*a*c
 	if (discriminant < 0)
 		return (false);
 	sqrt_d = sqrt(discriminant);
-	t = (ahc[1] - sqrt_d) / ahc[0];
-	if (t < 0)
-		t = (ahc[1] + sqrt_d) / ahc[0];
-	if (t < 0)
-		return (false);
-	out->distance = t;
-	out->point = vec3_add(ray.origin, vec3_multiply(ray.direction, t));
+	float q = (abc[1] > 0) ?
+            -0.5 * (abc[1] + sqrt_d) :
+            -0.5 * (abc[1] - sqrt_d);
+	float x0 = q / abc[0];
+	float x1 = abc[2] / q;
+	if (x0 > x1)
+	{
+		float tmp = x0;
+		x0 = x1;
+		x1 = tmp;
+	}
+	out->distance = (x0 < 0) ? x1 : x0;
+	// ray.orig + ray.dir * t
+	out->point = vec3_add(ray.origin, vec3_multiply(ray.direction, out->distance));
 	out->object = (t_object){.sphere = sphere, .type = SPHERE};
+	// -0.5 * (b - sqrt(disc)) / a
+	//t = -0.5 * (abc[1] - sqrt_d) / abc[0];
+	//if (t < 0)
+	//	t = (abc[1] + sqrt_d) / abc[0];
+	//if (t < 0)
+	//	return (false);
+	//out->distance = t;
+	//out->point = vec3_add(ray.origin, vec3_multiply(ray.direction, t));
+	//out->object = (t_object){.sphere = sphere, .type = SPHERE};
 	return (true);
 }
