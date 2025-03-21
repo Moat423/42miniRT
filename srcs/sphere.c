4f5e749d4165d6d7c76d64d08102e24b894ee204
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-03-21 12:45:04 +0100

    renamed 0 to B and 1 to C

--- srcs/sphere.c
+++ srcs/sphere.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kwurster <kwurster@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/19 11:01:02 by kwurster          #+#    #+#             */
-/*   Updated: 2025/03/21 12:35:47 by lmeubrin         ###   ########.fr       */
+/*   Created: 2025/03/9 11:01:02 by kwurster          #+#    #+#             */
+/*   Updated: 2025/03/21 12:44:22 by lmeubrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,33 +36,33 @@ bool	sphere_intersect(t_sphere *sphere, t_ray ray, t_intersection *out)
 	oc = vec3_subtract(ray.origin, sphere->pos);
 	// a = 1 (|normal vector|² = 1) = D²
 	// b = 2 * oc * D
-	bc[0] = 2 * vec3_dot(oc, ray.direction);
+	bc[B] = 2 * vec3_dot(oc, ray.direction);
 	// c = oc² - r²
-	bc[1] = vec3_squared_length(oc) - sphere->radius * sphere->radius;
+	bc[C] = vec3_squared_length(oc) - sphere->radius * sphere->radius;
 	// disc = b*b - 4*1*c
-	discriminant = bc[0] * bc[0] - 4 * bc[1];
+	discriminant = bc[B] * bc[B] - 4 * bc[C];
 	if (discriminant < 0)
 		return (false);
 	if (discriminant == 1.0)
 	{
 		// -b / 2*1
-		t[0] = -bc[0] * 0.5;
-		t[1] = t[0];
+		t[B] = -bc[B] * 0.5;
+		t[C] = t[B];
 	}
 	else
 	{
 		sqrt_d = sqrt(discriminant);
 		// (-b +- sqrt(disc)) / 2*1
-		t[0] = (-bc[0] - sqrt_d) * 0.5;
-		t[1] = (-bc[0] + sqrt_d) * 0.5;
+		t[B] = (-bc[B] - sqrt_d) * 0.5;
+		t[C] = (-bc[B] + sqrt_d) * 0.5;
 	}
 	// we want the closest intersection point thats not behind the camera/ ray origin
-	if (t[0] < 0)
-		t[0] = t[1];
+	if (t[B] < 0)
+		t[B] = t[C];
 	// if there is none we dont have an intersection in the ray's direction
-	if (t[0] < 0)
+	if (t[B] < 0)
 		return (false);
-	out->distance = t[0];
+	out->distance = t[B];
 	// P = O + t * D
 	out->point = vec3_add(ray.origin, vec3_multiply(ray.direction, out->distance));
 	out->object = (t_object){.sphere = sphere, .type = SPHERE};
