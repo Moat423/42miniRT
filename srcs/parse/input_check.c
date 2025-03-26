ecabb03a490b6a73bfff55bed5b7771d5ee14b11
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-03-26 12:48:27 +0100

    added headers

--- srcs/parse/input_check.c
+++ srcs/parse/input_check.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_check.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmeubrin <lmeubrin@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/26 12:47:40 by lmeubrin          #+#    #+#             */
+/*   Updated: 2025/03/26 12:47:57 by lmeubrin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../../include/miniRT.h"
 
@@ -6,7 +17,8 @@ int	input_check(int argc)
 	if (argc == 2)
 		return (1);
 	ft_fprintf(2, "usage: ./miniRT scene/scene-description.rt\n");
-	ft_fprintf(2, "scene-description.rt must be a valid file inside the 'scene' folder");
+	ft_fprintf(2, 
+		"scene-description.rt must be a valid file inside the 'scene' folder");
 	return (0);
 }
 
