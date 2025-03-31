bb2e3246c8d691cfaf5db3bbd121f25362312811
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-03-31 13:27:46 +0200

    put hypothenuse in library

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: kwurster <kwurster@student.42berlin.de>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/04/22 15:55:27 by lmeubrin          #+#    #+#              #
-#    Updated: 2025/03/27 15:58:23 by lmeubrin         ###   ########.fr        #
+#    Updated: 2025/03/31 13:25:55 by lmeubrin         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -77,6 +77,7 @@ SRCS := $(addprefix $(SRCS_DIR)/,\
 		plane.c \
 		cylinder.c \
 		light.c \
+		circle.c \
 		$(addprefix util/, color.c util.c vec3_0.c vec3_1.c vec3_2.c scene_malloc.c print_scene.c interval.c) \
 		$(addprefix parse/, input_check.c parse_scene.c set_scene.c set_objs.c set_utils.c) \
 		)
---
+++ srcs/circle.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   circle.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmeubrin <lmeubrin@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/31 12:57:20 by lmeubrin          #+#    #+#             */
+/*   Updated: 2025/03/31 12:58:44 by lmeubrin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/miniRT.h"
+
+bool	circle_intersect(t_circle circle, t_ray ray, 
+						float *o_dist, t_vec3 *o_pt)
+{
+	float	t;
+	float	denom;
+	t_vec3	hit_point;
+
+	denom = vec3_dot(circle.normal, ray.direction);
+	if (denom < EPSILON && denom > -EPSILON)
+		return (false);
+	t = vec3_dot(vec3_subtract(circle.pos, ray.origin), circle.normal)
+		/ denom;
+	if (!interval_contains(ray.range, t))
+		return (false);
+	hit_point = vec3_add(ray.origin, vec3_multiply(ray.direction, t));
+	// the (1.0f + EPSILON) is to account for floating point errors, can be taken out
+	if (vec3_squared_length(vec3_subtract(hit_point, circle.pos))
+		<= circle.radius * circle.radius * (1.0f + EPSILON))
+	{
+		*o_dist = t;
+		*o_pt = hit_point;
+		return (true);
+	}
+	return (false);
+}
+
+bool	closer_circle_intersect(t_cylinder *cylinder, t_ray ray, 
+								float *o_dist, t_vec3 *o_pt)
+{
+	t_vec3	hit_point[2];
+	float	hit_dist[2];
+
+	// if (vec3_squared_length(vec3_subtract(cylinder->top, ray.origin)) <= vec3_squared_length(vec3_subtract(cylinder->bottom, ray.origin)))
+	if (!circle_intersect((t_circle){cylinder->top, cylinder->axis, cylinder->radius}, ray, &(hit_dist[0]), &(hit_point[0])))
+		return (circle_intersect((t_circle){cylinder->bottom, cylinder->axis, cylinder->radius}, ray, o_dist, o_pt));
+	if (!circle_intersect((t_circle){cylinder->bottom, cylinder->axis, cylinder->radius}, ray, &(hit_dist[1]), &(hit_point[1])))
+	{
+		*o_dist = hit_dist[0];
+		*o_pt = hit_point[0];
+		return (true);
+	}
+	if (hit_dist[0] < hit_dist[1])
+	{
+		*o_dist = hit_dist[0];
+		*o_pt = hit_point[0];
+		return (true);
+	}
+	*o_dist = hit_dist[1];
+	*o_pt = hit_point[1];
+	return (true);
+}
