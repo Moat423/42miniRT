d6ca9bbda3f52a9049e941b6db6675b869eabe2b
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-03-31 14:09:42 +0200

    trying to extract malloc

--- srcs/util/scene_malloc.c
+++ srcs/util/scene_malloc.c
@@ -1,14 +1,29 @@
 #include "../../include/miniRT_types.h"
 #include "../../include/parse.h"
 
+// returns 0 on failure and 
+// 1 if it was sucessful or didn't alloc annything
+int	ft_malloc_array(const size_t count, const size_t size, void *arr)
+{
+	if (count)
+	{
+		arr = malloc(count * sizeof(t_sphere));
+		if (!arr)
+			return (0);
+	}
+	return (1);
+}
+
 //return 0 on faulure also checks if at least one light in scene
 int	ft_malloc_scene_arrays(t_scene *scene)
 {
 	if (scene->light_count == 0)
 		return (ft_parseerror("Need Lights! Give at least one L", NULL));
-	scene->lights = malloc(scene->light_count * sizeof(t_light));
-	if (!scene->lights)
+	if (!ft_malloc_array(scene->light_count, sizeof(t_light), &(scene->lights)))
 		return (0);
+	// scene->lights = malloc(scene->light_count * sizeof(t_light));
+	// if (!scene->lights)
+	// 	return (0);
 	if (scene->sphere_count)
 	{
 		scene->spheres = malloc(scene->sphere_count * sizeof(t_sphere));
