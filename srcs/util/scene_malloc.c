86bcf0231185ee6ae853d6735d517e6d8848b653
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-03-31 14:20:31 +0200

    extracted malloc

--- srcs/util/scene_malloc.c
+++ srcs/util/scene_malloc.c
@@ -3,12 +3,12 @@
 
 // returns 0 on failure and 
 // 1 if it was sucessful or didn't alloc annything
-int	ft_malloc_array(const size_t count, const size_t size, void *arr)
+int	ft_malloc_array(const size_t count, const size_t size, void **arr)
 {
 	if (count)
 	{
-		arr = malloc(count * sizeof(t_sphere));
-		if (!arr)
+		*arr = malloc(count * size);
+		if (!*arr)
 			return (0);
 	}
 	return (1);
@@ -19,28 +19,20 @@ int	ft_malloc_scene_arrays(t_scene *scene)
 {
 	if (scene->light_count == 0)
 		return (ft_parseerror("Need Lights! Give at least one L", NULL));
-	if (!ft_malloc_array(scene->light_count, sizeof(t_light), &(scene->lights)))
+	if (!ft_malloc_array(scene->light_count, sizeof(t_light), 
+			(void **)&(scene->lights)))
 		return (0);
-	// scene->lights = malloc(scene->light_count * sizeof(t_light));
-	// if (!scene->lights)
+	if (!ft_malloc_array(scene->sphere_count, sizeof(t_sphere), 
+			(void **)&(scene->spheres)))
+		return (0);
+	if (!ft_malloc_array(scene->plane_count, sizeof(t_plane), 
+			(void **)&(scene->planes)))
+		return (0);
+	if (!ft_malloc_array(scene->cylinder_count, sizeof(t_cylinder), 
+			(void **)&(scene->cylinders)))
+		return (0);
+	// if (!ft_malloc_array(s->cone_count, sizeof(t_cone), 
+	// (void **)&(s->cones)))
 	// 	return (0);
-	if (scene->sphere_count)
-	{
-		scene->spheres = malloc(scene->sphere_count * sizeof(t_sphere));
-		if (!scene->spheres)
-			return (0);
-	}
-	if (scene->plane_count)
-	{
-		scene->planes = malloc(scene->plane_count * sizeof(t_plane));
-		if (!scene->planes)
-			return (0);
-	}
-	if (scene->cylinder_count)
-	{
-		scene->cylinders = malloc(scene->cylinder_count * sizeof(t_cylinder));
-		if (!scene->cylinders)
-			return (0);
-	}
 	return (1);
 }
