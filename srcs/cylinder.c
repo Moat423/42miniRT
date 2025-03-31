b95ccbaa96744f20678fd87e1d93ed6e50ae4464
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-03-31 13:40:13 +0200

    took out circle functions

--- srcs/cylinder.c
+++ srcs/cylinder.c
@@ -6,15 +6,13 @@
 /*   By: kwurster <kwurster@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 18:38:32 by lmeubrin          #+#    #+#             */
-/*   Updated: 2025/03/31 10:43:22 by lmeubrin         ###   ########.fr       */
+/*   Updated: 2025/03/31 13:25:31 by lmeubrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/miniRT.h"
 
-bool	circle_intersect(t_circle circle, t_ray ray, float *out_dist, t_vec3 *out_point);
-bool	closer_circle_intersect(t_cylinder *cylinder, t_ray ray, float *o_dist, t_vec3 *o_pt);
-float	get_discriminant(float abc[3], const t_ray ray, const t_cylinder *cyl);
+static	float	get_discriminant(float abc[3], const t_ray ray, const t_cylinder *cyl);
 
 /// @brief Calculate the unnormalized surface normal of a cylinder at a given
 /// intersection point
@@ -97,69 +95,18 @@ bool	cylinder_intersect(t_cylinder *cylinder, t_ray ray, t_intersection *out)
 	return (true);
 }
 
-float	get_discriminant(float abc[3], const t_ray ray, const t_cylinder *cyl)
+static	float	get_discriminant(float abc[3], const t_ray ray, const t_cylinder *cyl)
 {
 	t_vec3	oc;
+	float	d_dot_ax;
+	float	oc_dot_ax;
 
 	oc = vec3_subtract(ray.origin, cyl->pos);
-	abc[A] = vec3_squared_length(ray.direction) 
-		-vec3_dot(ray.direction, cyl->axis) 
-		* vec3_dot(ray.direction, cyl->axis);
-	abc[C] = vec3_squared_length(oc) - (vec3_dot(oc, cyl->axis) 
-			* vec3_dot(oc, cyl->axis)) - cyl->radius * cyl->radius;
+	d_dot_ax = vec3_dot(ray.direction, cyl->axis);
+	oc_dot_ax = vec3_dot(oc, cyl->axis);
+	abc[A] = vec3_squared_length(ray.direction) - d_dot_ax * d_dot_ax;
+	abc[C] = vec3_squared_length(oc) - (oc_dot_ax * oc_dot_ax) - cyl->radius * cyl->radius;
 	abc[B] = 2 * (vec3_dot(ray.direction, oc) 
-			- vec3_dot(ray.direction, cyl->axis) * vec3_dot(oc, cyl->axis));
+			- d_dot_ax * oc_dot_ax);
 	return (abc[B] * abc[B] - 4 * abc[A] * abc[C]);
-}
-
-bool	closer_circle_intersect(t_cylinder *cylinder, t_ray ray, 
-								float *o_dist, t_vec3 *o_pt)
-{
-	t_vec3	hit_point[2];
-	float	hit_dist[2];
-
-	// if (vec3_squared_length(vec3_subtract(cylinder->top, ray.origin)) <= vec3_squared_length(vec3_subtract(cylinder->bottom, ray.origin)))
-	if (!circle_intersect((t_circle){cylinder->top, cylinder->axis, cylinder->radius}, ray, &(hit_dist[0]), &(hit_point[0])))
-		return (circle_intersect((t_circle){cylinder->bottom, cylinder->axis, cylinder->radius}, ray, o_dist, o_pt));
-	if (!circle_intersect((t_circle){cylinder->bottom, cylinder->axis, cylinder->radius}, ray, &(hit_dist[1]), &(hit_point[1])))
-	{
-		*o_dist = hit_dist[0];
-		*o_pt = hit_point[0];
-		return (true);
-	}
-	if (hit_dist[0] < hit_dist[1])
-	{
-		*o_dist = hit_dist[0];
-		*o_pt = hit_point[0];
-		return (true);
-	}
-	*o_dist = hit_dist[1];
-	*o_pt = hit_point[1];
-	return (true);
-}
-
-bool	circle_intersect(t_circle circle, t_ray ray, 
-						float *o_dist, t_vec3 *o_pt)
-{
-	float	t;
-	float	denom;
-	t_vec3	hit_point;
-
-	denom = vec3_dot(circle.normal, ray.direction);
-	if (denom < EPSILON && denom > -EPSILON)
-		return (false);
-	t = vec3_dot(vec3_subtract(circle.pos, ray.origin), circle.normal)
-		/ denom;
-	if (!interval_contains(ray.range, t))
-		return (false);
-	hit_point = vec3_add(ray.origin, vec3_multiply(ray.direction, t));
-	// the (1.0f + EPSILON) is to account for floating point errors, can be taken out
-	if (vec3_squared_length(vec3_subtract(hit_point, circle.pos))
-		<= circle.radius * circle.radius * (1.0f + EPSILON))
-	{
-		*o_dist = t;
-		*o_pt = hit_point;
-		return (true);
-	}
-	return (false);
 }
