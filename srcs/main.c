3da770e1d37cf3cc1a7356a7c8a528a13b1b60d7
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-04-03 14:27:26 +0200

    freeing scene in case of parse error

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: kwurster <kwurster@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 13:14:02 by lmeubrin          #+#    #+#             */
-/*   Updated: 2025/04/02 11:25:04 by lmeubrin         ###   ########.fr       */
+/*   Updated: 2025/04/03 14:22:37 by lmeubrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,12 +41,11 @@ int	main(int argc, char **argv)
 	minirt = (t_minirt){0};
 	minirt.last_render_request_time = INFINITY;
 	scene = &minirt.scene;
-	(void)argc;
-	(void)argv;
-	if (input_check(argc) == 0)
+	if (!(input_check(argc) && !parse_scene(argv[1], scene)))
+	{
+		scene_destroy(scene);
 		return (1);
-	if (parse_scene(argv[1], scene))
-		return (1);
+	}
 	print_scene(scene);
 	scene->image_width = 800;
 	scene->image_height = 600;
