926f66dae441f2ce7a02b0beabb0a97e2a85613e
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-04-15 13:05:09 +0200

    refactored a bit to pass right direction and up direction

--- srcs/movement.c
+++ srcs/movement.c
@@ -6,7 +6,7 @@
 /*   By: lmeubrin <lmeubrin@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 13:19:53 by lmeubrin          #+#    #+#             */
-/*   Updated: 2025/04/14 14:16:59 by lmeubrin         ###   ########.fr       */
+/*   Updated: 2025/04/15 13:04:30 by lmeubrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,7 @@ static t_vec3	update_camera_position(t_camera camera,
 	return (vec3_add(camera.pos, vec3_multiply(dir, speed)));
 }
 
-static bool	main_key_movement(t_minirt *minirt, t_vec3 right_direction,
-							t_vec3 up_direction)
+static bool	main_key_movement(t_minirt *minirt, t_vec3 right_direction)
 {
 	t_camera	*camera;
 
@@ -41,16 +40,13 @@ static bool	main_key_movement(t_minirt *minirt, t_vec3 right_direction,
 	return (true);
 }
 
-static bool	key_movement(t_minirt *minirt)
+static bool	key_movement(t_minirt *minirt,
+						t_vec3 right_direction, t_vec3 up_direction)
 {
 	t_camera	*camera;
-	t_vec3		right_direction;
-	t_vec3		up_direction;
 
 	camera = &minirt->scene.camera;
-	right_direction = vec3_cross(camera->dir, camera->up);
-	up_direction = vec3_cross(camera->dir, camera->right);
-	if (main_key_movement(minirt, right_direction, up_direction))
+	if (main_key_movement(minirt, right_direction))
 		return (true);
 	else if (mlx_is_key_down(minirt->mlx, MLX_KEY_Q))
 		camera->pos = update_camera_position(*camera, up_direction, -SPEED);
@@ -60,13 +56,48 @@ static bool	key_movement(t_minirt *minirt)
 		return (false);
 	return (true);
 }
+//
+// static	bool	camera_movement(t_minirt *minirt,
+// 								t_vec3 right_direction, t_vec3 up_direction)
+// {
+// 	int		delta_x;
+// 	int		delta_y;
+// 	int32_t	new_x;
+// 	int32_t	new_y;
+// 	t_camera	*camera;
+//
+// 	camera = &minirt->scene.camera;
+// 	mlx_get_mouse_pos(minirt->mlx,
+// 		&(minirt->mouse_pos_x), &(minirt->mouse_pos_y));
+// 	if (!mlx_is_mouse_down(minirt->mlx, MLX_MOUSE_BUTTON_RIGHT))
+// 		return (false);
+// 	mlx_get_mouse_pos(minirt->mlx,
+// 		&(new_x), &(new_y));
+// 	delta_x = minirt->mouse_pos_x - new_x;
+// 	delta_y = minirt->mouse_pos_y - new_y;
+// 	if (delta_x != 0 || delta_y != 0)
+// 	{
+// 		float	pitch_delta = delta_y * SPEED;
+// 		float	yaw_delta = delta_x * SPEED;
+// 		// quaternion q = normalise(cross(angleAxis(-pitch_delta, right_direction), angleAxis(-yaw_delta, up_direction)))
+// 		// camera->dir = rotate(q, camera->dir);
+// 		return (true);
+// 	}
+// 	return (false);
+// }
 
 static void	movement(t_minirt *minirt)
 {
 	t_scene		*scene;
+	t_vec3		right_direction;
+	t_vec3		up_direction;
 
+	right_direction = vec3_cross(minirt->scene.camera.dir,
+			minirt->scene.camera.up);
+	up_direction = vec3_cross(minirt->scene.camera.dir,
+			minirt->scene.camera.right);
 	scene = &minirt->scene;
-	if (key_movement(minirt))
+	if (key_movement(minirt, right_direction, up_direction))
 		minirt->loop_state = DEFERRED_RENDER;
 	else if (minirt->loop_state == DEFERRED_RENDER)
 		minirt->loop_state = RENDER_NOW;
