f9dfac02e1d50fdb4a618e85076672253cdf7dbe
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-04-29 15:03:54 +0200

    extracting functions (line limit norme)

--- srcs/scene_fixing.c
+++ srcs/scene_fixing.c
@@ -1,4 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   scene_fixing.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmeubrin <lmeubrin@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 14:59:10 by lmeubrin          #+#    #+#             */
+/*   Updated: 2025/04/29 14:59:11 by lmeubrin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/miniRT.h"
+
+static void	set_derived_camera(t_scene *scene)
+{
+	static const t_vec3	world_up = {0, 1, 0};
+
+	scene->camera.right = vec3_cross(scene->camera.dir, world_up);
+	if (vec3_squared_length(scene->camera.right) < EPSILON * EPSILON)
+		scene->camera.right = vec3_cross(scene->camera.dir, vec3_new(1, 0, 0));
+	scene->camera.right = vec3_normalize(scene->camera.right);
+	scene->camera.up = vec3_normalize(vec3_cross(scene->camera.right,
+				scene->camera.dir));
+}
+
+static void	set_derived_cylinders(t_scene *scene)
+{
+	size_t				i;
+	t_cylinder			*cyl;
+
+	i = 0;
+	while (i < scene->objs.cylinder_count)
+	{
+		cyl = &scene->objs.cylinders[i];
+		cyl->top = vec3_add(cyl->pos, vec3_multiply(cyl->axis,
+					cyl->height / 2.0f));
+		cyl->bottom = vec3_add(cyl->pos, vec3_multiply(cyl->axis,
+					-cyl->height / 2.0f));
+		i++;
+	}
+}
 
 /**
  * @brief Recalculates scene values that depend on other parsed values,
@@ -12,24 +53,9 @@ void	calculate_derived_scene_values(t_scene *scene)
 	size_t				i;
 	t_cylinder			*cyl;
 	t_cone				*cone;
-	static const t_vec3	world_up = {0, 1, 0};
 
-	scene->camera.right = vec3_cross(scene->camera.dir, world_up);
-	if (vec3_squared_length(scene->camera.right) < EPSILON * EPSILON)
-		scene->camera.right = vec3_cross(scene->camera.dir, vec3_new(1, 0, 0));
-	scene->camera.right = vec3_normalize(scene->camera.right);
-	scene->camera.up = vec3_normalize(vec3_cross(scene->camera.right,
-				scene->camera.dir));
-	i = 0;
-	while (i < scene->objs.cylinder_count)
-	{
-		cyl = &scene->objs.cylinders[i];
-		cyl->top = vec3_add(cyl->pos, vec3_multiply(cyl->axis,
-					cyl->height / 2.0f));
-		cyl->bottom = vec3_add(cyl->pos, vec3_multiply(cyl->axis,
-					-cyl->height / 2.0f));
-		i++;
-	}
+	set_derived_camera(scene);
+	set_derived_cylinders(scene);
 	i = 0;
 	while (i < scene->objs.cone_count)
 	{
