02c6d9d9de1d5885c4c8442d75e924dc2708f32b
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-05-07 15:32:01 +0200

    fixed leaking bumpmap

--- srcs/util/scene_destroy.c
+++ srcs/util/scene_destroy.c
@@ -6,7 +6,7 @@
 /*   By: lmeubrin <lmeubrin@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 12:30:26 by lmeubrin          #+#    #+#             */
-/*   Updated: 2025/05/05 19:34:03 by lmeubrin         ###   ########.fr       */
+/*   Updated: 2025/05/07 15:31:17 by lmeubrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ static void	objects_destroy_inner(t_objects *objects)
 	while (i < objects->sphere_count)
 	{
 		free(objects->spheres[i].lights);
-		if (objects->spheres->texture == BUMP)
+		if (objects->spheres[i].texture == BUMP)
 			bumpmap_destroy(objects->spheres[i].bumpmap);
 		i++;
 	}
