ddc39646a6cf0cfd092c1db41a358da2c077fba9
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-05-12 14:20:36 +0200

    added filtered elevation, step size in uv space (needs refactor)

--- srcs/bumpmap_apply.c
+++ srcs/bumpmap_apply.c
@@ -6,7 +6,7 @@
 /*   By: lmeubrin <lmeubrin@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 16:10:21 by lmeubrin          #+#    #+#             */
-/*   Updated: 2025/05/12 12:26:14 by lmeubrin         ###   ########.fr       */
+/*   Updated: 2025/05/12 14:17:00 by lmeubrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,36 @@ float	get_bump_elevation(const t_bumpmap *bump, const t_point point)
 	return (bump->elevation[index]);
 }
 
+// Bilinear sampling function
+float get_filtered_bump_elevation(const t_bumpmap *bump, const t_point uv)
+{
+    float u = uv.u * (bump->width - 1);
+    float v = uv.v * (bump->height - 1);
+    
+    // Get integer coordinates
+    int x0 = (int)floorf(u);
+    int y0 = (int)floorf(v);
+    int x1 = fmin(x0 + 1, bump->width - 1);
+    int y1 = fmin(y0 + 1, bump->height - 1);
+    
+    // Get fractional parts
+    float u_frac = u - x0;
+    float v_frac = v - y0;
+    
+    // Sample all four points
+    float h00 = bump->elevation[y0 * bump->width + x0];
+    float h10 = bump->elevation[y0 * bump->width + x1];
+    float h01 = bump->elevation[y1 * bump->width + x0];
+    float h11 = bump->elevation[y1 * bump->width + x1];
+    
+    // Bilinear interpolation
+    float h0 = h00 * (1 - u_frac) + h10 * u_frac;
+    float h1 = h01 * (1 - u_frac) + h11 * u_frac;
+    
+    return h0 * (1 - v_frac) + h1 * v_frac;
+}
+
+
 /*
 ** Calculate bump map normal perturbation
 ** Returns the perturbed normal vec3
@@ -48,17 +78,18 @@ t_vec3	apply_bump_mapping(const t_sphere *sphere,
 		return (normal);
 	point = spherical_map(intersection_point, sphere->radius);
 	// Sample height at current point and neighboring points
-	height = get_bump_elevation(sphere->bumpmap, point);
+	height = get_filtered_bump_elevation(sphere->bumpmap, point);
 	// Get height differences (derivatives)
 	// Sample neighboring points for derivatives
 	// Using small offset for UV coordinates
 	t_point point_u = point;
 	t_point point_v = point;
+	float uv_step = 0.0005; // Constant step in UV space
+	point_u.u = fmod(point.u + uv_step, 1.0f); // Use fmod to handle wrapping
+	point_v.v = fmod(point.v + uv_step, 1.0f);
 	// Offset points slightly in u and v directions
-	point_u.u = fmin(point.u + 1.0f / sphere->bumpmap->width, 1.0f);
-	point_v.v = fmin(point.v + 1.0f / sphere->bumpmap->height, 1.0f);
-	h_point.u = get_bump_elevation(sphere->bumpmap, point_u) - height;
-	h_point.v = get_bump_elevation(sphere->bumpmap, point_v) - height;
+	h_point.u = get_filtered_bump_elevation(sphere->bumpmap, point_u) - height;
+	h_point.v = get_filtered_bump_elevation(sphere->bumpmap, point_v) - height;
 	// h_point.u = get_bump_elevation(sphere->bumpmap, (t_point){
 	// 		fmin(point.u + 1.0f/sphere->bumpmap->width, 1.0f), point.v}) - height;
 	// h_point.v = get_bump_elevation(sphere->bumpmap, (t_point){point.u, 
@@ -77,14 +108,14 @@ t_vec3	apply_bump_mapping(const t_sphere *sphere,
 		cos(phi)
 	});
 	// bitangent simple calc:
-	bitangent = vec3_normalize(vec3_cross(normal, tangent));
+	// bitangent = vec3_normalize(vec3_cross(normal, tangent));
 	// Bitangent (along lines of latitude)
-	// float theta = M_PI * point.v;
-	// bitangent = vec3_normalize((t_vec3){
-	// 	cos(phi) * cos(theta),
-	// 	-sin(theta),
-	// 	sin(phi) * cos(theta)
-	// });
+	float theta = M_PI * point.v;
+	bitangent = vec3_normalize((t_vec3){
+		cos(phi) * cos(theta),
+		-sin(theta),
+		sin(phi) * cos(theta)
+	});
 	// Perturb the normal using the height derivatives
 	normal = vec3_normalize(
 			vec3_add(normal, 
