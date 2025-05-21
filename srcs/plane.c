3e3558f11c9327ce44de1aeca443b0f821eb5884
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-05-21 15:03:30 +0200

    disabled bumpmaps for planes(gives warning, doesn't show bp)

--- srcs/plane.c
+++ srcs/plane.c
@@ -6,12 +6,17 @@
 /*   By: kwurster <kwurster@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 13:51:03 by lmeubrin          #+#    #+#             */
-/*   Updated: 2025/05/21 13:13:09 by lmeubrin         ###   ########.fr       */
+/*   Updated: 2025/05/21 14:39:06 by lmeubrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/miniRT.h"
 
+// geturn an intersection with the plane if there is one
+//
+// to enable bumpmaps use:
+// out->normal = plane_apply_bump_mapping(plane, out->point,
+// 		out->normal);
 bool	plane_intersect(t_plane *plane, t_ray ray, t_intersection *out)
 {
 	float	t;
@@ -29,8 +34,6 @@ bool	plane_intersect(t_plane *plane, t_ray ray, t_intersection *out)
 		out->object.type = PLANE;
 		out->point = vec3_add(ray.origin, vec3_multiply(ray.direction, t));
 		out->normal = plane->normal;
-		out->normal = plane_apply_bump_mapping(plane, out->point,
-				out->normal);
 		out->normal_calculated = true;
 		return (true);
 	}
